b37035e6f04eeb7d642c88290b9c5351d6190d17
XuFengthucs <36250097+XuFengthucs@users.noreply.github.com>
2023-02-02 17:08:22 +0800

    Update testSVT_pic.m

--- SVT/testSVT_pic.m
+++ SVT/testSVT_pic.m
@@ -7,6 +7,6 @@ parfor i = 1
 end
 t = cputime;
 %[X, iters, k] = SVT(M, 0.1, [0 1]);
-[X, iters, k] = fastSVT_Q(M, 0.1, [0 1], 50, 10);
+[X, iters, k] = fastSVT_U(M, 0.1, [0 1], 50, 10);
 t_SVT = cputime - t
 err = sum(sum(abs([pic(:,:,1);pic(:,:,2);pic(:,:,3)]-X)))/2048/2048/3*255
